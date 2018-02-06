4f4ba3f4b06410e89f22879dfb2276bfe32c3249
Thibaud Marcé <thibaud.marce@gmail.com>
2018-02-06 23:32:59 +0100

    created method that returns all the tracks longer than a min_length

--- app/queries.rb
+++ app/queries.rb
@@ -10,6 +10,7 @@ end
 
 def long_tracks(min_length)
   # TODO: return an array of Track of at least `min_length` minutes
+  Track.where("milliseconds > ?", min_length * 60000)
 end
 
 def top_five_artists(genre_name)
