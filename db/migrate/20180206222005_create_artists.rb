e8ed815a78aa51bd6c679298f39584f8c7b9955c
Thibaud Marcé <thibaud.marce@gmail.com>
2018-02-06 22:34:16 +0100

    created artist model inheriting from active record base

--- db/migrate/20180206222005_create_artists.rb
+++
@@ -1,8 +0,0 @@
-class CreateRestaurants < ActiveRecord::Migration[5.1]
-  def change
-    create_table :artists do |t|
-      t.string    :name
-      t.timestamps # add 2 columns, `created_at` and `updated_at`
-    end
-  end
-end
